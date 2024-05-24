5877c5592faa8eecc57268148d18cbcf62f3c4fe
Daniel Moreno <danielmoreno@Daniels-MacBook-Air.local>
2024-05-24 12:23:25 +0200

    classifier done

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -2,38 +2,124 @@ import os
 import re
 import shutil
 import PyPDF2
+import time
+class CLassifier:
+    def __init__(self, source_folder, pattern, target_folder):
+        start = time.time()
+        self.source_folder = source_folder
+        self.pattern = pattern
+        self.target_folder = target_folder
+        # Call the filter_pdfs_by_pattern method with the pattern
+        self.filter_pdfs_by_pattern(self.pattern)
+        end = time.time()
+        self.execution_time = end - start
 
-def read_pdf(file_path):
-    """Reads PDF file and returns the text content."""
-    with open(file_path, 'rb') as f:
-        pdf = PyPDF2.PdfReader(f)
-        text = ''
-        for i in range(len(pdf.pages)):
-            text += pdf.pages[i].extract_text()
-    return text
+        # Get date and time like 'dd-mm-yyyy HH:MM:SS'
+        self.execution_date = time.strftime('%Y-%m-%d %H:%M:%S')
 
-def filter_text(text, pattern):
-    """Finds all occurrences of the pattern in text."""
-    return re.findall(pattern, text)
+    # Methods to read PDFs and filter text
+    def read_pdf(self, file_path):
+        """Reads PDF file and returns the text content."""
+        with open(file_path, 'rb') as f:
+            pdf = PyPDF2.PdfReader(f)
+            text = ''
+            for i in range(len(pdf.pages)):
+                text += pdf.pages[i].extract_text()
+        return text
 
-def filter_pdfs_by_pattern(pattern, source_folder, target_folder):
-    """Filters PDFs by pattern and copies matching PDFs to a new directory.
-    Possible patterns: r'A\. Contratación del Sector Público', r'Anuncio de formalización', r'Formalización del contrato'
+    def filter_text(self, text, pattern):
+        """Finds all occurrences of the pattern in text."""
+        return re.findall(pattern, text)
+
+    def filter_pdfs_by_pattern(self, pattern):
+        """Filters PDFs by pattern and copies matching PDFs to a new directory.
+        Possible patterns: r'A\. Contratación del Sector Público', r'Anuncio de formalización', r'Formalización del contrato'
+        """
+        # Create target directory if it does not exist
+        if not os.path.exists(self.target_folder):
+            os.makedirs(self.target_folder)
+
+        filtered_files = []
+        for file in os.listdir(self.source_folder):
+            if file.endswith('.pdf'):
+                full_path = os.path.join(self.source_folder, file)
+                text = self.read_pdf(full_path)
+                matches = self.filter_text(text, pattern)
+                if matches:
+                    # Print matched files and the found text
+                    print(f'{file}: {matches}')
+                    # move to target folder
+                    shutil.move(full_path, os.path.join(self.target_folder, file))
+                    filtered_files.append(file)
+
+class ClassifierContratacion(CLassifier):
+    def __init__(self, source_folder):
+        pattern_contratacion = r'A\. Contratación del Sector Público'
+        target_folder = os.path.join(source_folder, 'contratacion')
+
+        # Number of files in the source folder
+        self.num_all_pdfs = len(os.listdir(source_folder))
+
+
+        super().__init__(source_folder, pattern_contratacion, target_folder)
+
+        # Number of files in the target folder. It needs to be calculated after the filtering because the target folder is created in the parent class
+        self.num_contratacion_pdfs = len(os.listdir(target_folder))
+        # Call the delete_source_folder method
+        self.delete_source_folder()
+
+        # Get mm_yyyy from the source folder
+        self.mm_yyyy = source_folder.split('_')[-1]
+
+    def delete_source_folder(self):
+        """Deletes all pdfs in the source folder after filtering."""
+        for file in os.listdir(self.source_folder):
+            if file.endswith('.pdf'):
+                os.remove(os.path.join(self.source_folder, file))
+
+class ClassifierAnuncio(CLassifier):
+    def __init__(self, source_folder):
+        pattern_contratacion = r'Anuncio de formalización'
+        parent_folder = os.path.dirname(source_folder)
+        target_folder = os.path.join(parent_folder, 'anuncio')
+
+        super().__init__(source_folder, pattern_contratacion, target_folder)
+
+        # Number of files in target folder. It needs to be calculated after the filtering because the target folder is created in the parent class
+        self.num_anuncio_pdfs = len(os.listdir(target_folder))
+
+class ClassifierFormalizacion(CLassifier):
+    def __init__(self, source_folder):
+        pattern_contratacion = r'Formalización del contrato'
+        parent_folder = os.path.dirname(source_folder)
+        target_folder = os.path.join(parent_folder, 'formalizacion')
+
+
+        super().__init__(source_folder, pattern_contratacion, target_folder)
+
+        # Number of files in target folder. It needs to be calculated after the filtering because the target folder is created in the parent class
+        self.num_formalizacion_pdfs = len(os.listdir(target_folder))
+
+# Save the execution time of the three classes in classify_time.csv with the following columns: mm_yyyy,num_all_pdfs,time_contratacion,num_contratacion_pdfs,time_anuncio,num_anuncio_pdfs,time_formalizacion,num_formalizacion_pdfs,execution_date
+def save_execution_time(classifier_contratacion, classifier_anuncio, classifier_formalizacion):
+    """Save the execution time of the three classes in classify_time.csv.
+
+    Args:
+        classifier_contratacion: Instance of ClassifierContratacion.
+        classifier_anuncio: Instance of ClassifierAnuncio.
+        classifier_formalizacion: Instance of ClassifierFormalizacion.
     """
-    # Create target directory if it does not exist
-    if not os.path.exists(target_folder):
-        os.makedirs(target_folder)
+    with open("classify_time.csv", 'a') as f:
+        f.write(f"\n{classifier_contratacion.mm_yyyy},{classifier_contratacion.num_all_pdfs},{classifier_contratacion.execution_time},{classifier_contratacion.num_contratacion_pdfs},{classifier_anuncio.execution_time},{classifier_anuncio.num_anuncio_pdfs},{classifier_formalizacion.execution_time},{classifier_formalizacion.num_formalizacion_pdfs},{classifier_contratacion.execution_date}")
 
-    filtered_files = []
-    for file in os.listdir(source_folder):
-        if file.endswith('.pdf'):
-            full_path = os.path.join(source_folder, file)
-            text = read_pdf(full_path)
-            matches = filter_text(text, pattern)
-            if matches:
-                # Print matched files and the found text
-                print(f'{file}: {matches}')
-                # move to target folder
-                shutil.move(full_path, os.path.join(target_folder, file))
-                filtered_files.append(file)
-    return filtered_files
+# Test the three classes and save the execution time
+source_folder_contratacion = 'pdfs_range_08-2016'
+classifier_contratacion = ClassifierContratacion(source_folder=source_folder_contratacion)
+
+source_folder_anuncio = 'pdfs_range_08-2016/contratacion'
+classifier_anuncio = ClassifierAnuncio(source_folder=source_folder_anuncio)
+
+source_folder_formalizacion = 'pdfs_range_08-2016/contratacion'
+classifier_formalizacion = ClassifierFormalizacion(source_folder=source_folder_formalizacion)
+
+save_execution_time(classifier_contratacion, classifier_anuncio, classifier_formalizacion)
--- readme.md
+++ readme.md
@@ -24,6 +24,28 @@ Let's proceed with the data extraction. We have the file `xml_departamento_id_fe
 
 Now, let's proceed with downloading the desired PDFs.
 
+Initially, a directory named `pdfs_range_mm-yyyy/` is created to contain all PDF files from a specific month and year. Subsequently, a subdirectory `contratacion/` is established within `pdfs_range_mm-yyyy/` to house PDFs associated with public sector contracts. These PDFs are identified by their content containing the phrase 'A Contratación del Sector Público'. Following the same method, two additional subdirectories, `anuncio/` and `formalizacion/`, are created within `pdfs_range_mm-yyyy/` to accommodate PDFs related to contract formalization announcements and contract formalization, respectively. This hierarchical organization results in the following folder structure:
+
+```
+pdfs_range_mm-yyyy/
+│
+├── contratacion/
+│   ├── pdf_file_1.pdf
+│   ├── pdf_file_2.pdf
+│   ├── ...
+│
+├── anuncio/
+│   ├── pdf_file_1.pdf
+│   ├── pdf_file_2.pdf
+│   ├── ...
+│
+└── formalizacion/
+    ├── pdf_file_1.pdf
+    ├── pdf_file_2.pdf
+    ├── ...
+```
+
+All pdfs not related to public sector contrats are removed.
 
 # Download pdfs
 In `download/download.py`, we define a class `Download` with the following attributes:
@@ -45,3 +67,42 @@ The rows are added with `\n` at the beggining.
 start_date,end_date,number_requests,number_pdfs,time,execution_date
 ```
 
+# Classify pdfs
+`classifier/classifier.py` is a module for classifying PDF files based on specified patterns.
+
+This module contains classes for classifying PDF files based on patterns in their text content.
+It provides functionality to read PDFs, filter text, and move matching PDFs to designated folders.
+
+Classes:
+    - `Classifier`: Base class for classifying PDFs based on a given pattern.
+    - `ClassifierContratacion`: Class for classifying PDFs related to public sector contracts.
+    - `ClassifierAnuncio`: Class for classifying PDFs related to contract formalization announcements.
+    - `ClassifierFormalizacion`: Class for classifying PDFs related to contract formalization.
+
+Usage:
+    - Instantiate one of the classifier classes with the source folder containing PDFs to classify.
+    - The classifier will move PDFs matching the specified pattern to the designated target folder.
+
+Attributes:
+    - source_folder: The source folder containing the PDF files to be classified.
+    - pattern: The pattern used to filter PDF text content.
+    - target_folder: The target folder where matched PDFs will be moved.
+    - execution_time: Time taken for classification process.
+    - execution_date: Date and time of classification process execution.
+    - Number of total all pdfs of the month, all pdfs related to public sector contrats, announcements and formalizations.
+save_execution_time function saves the execution time of the three classes in classify_time.csv.
+- Args:
+    - `classifier_contratacion`: Instance of ClassifierContratacion.
+    - `classifier_anuncio`: Instance of ClassifierAnuncio.
+    - `classifier_formalizacion`: Instance of ClassifierFormalizacion.
+
+The `classify_time.csv` file contains the following columns:
+- `mm_yyyy`: Month and year of classified PDFs.
+- `num_all_pdfs`: Total number of PDFs before classification.
+- `time_contratacion`: Time taken for classifying public sector contract PDFs.
+- `num_contratacion_pdfs`: Number of PDFs classified under public sector contracts.
+- `time_anuncio`: Time taken for classifying contract formalization announcement PDFs.
+- `num_anuncio_pdfs`: Number of PDFs classified as contract formalization announcements.
+- `time_formalizacion`: Time taken for classifying contract formalization PDFs.
+- `num_formalizacion_pdfs`: Number of PDFs classified as contract formalization documents.
+- `execution_date`: Date and time of execution.
