a797a73e96568d6b7ea451ca9f7ee630c07693d1
Daniel Moreno <danielmoreno@Daniels-MacBook-Air.local>
2024-06-18 22:10:24 +0200

    logging added to main.py

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ if not os.path.exists(log_dir):
 
 # Pass log_dir to other scripts
 log_dir_path = os.path.abspath(log_dir)
+# Set up logging
+import logging
+logging.basicConfig(filename=os.path.join(log_dir, 'main.log'),
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # Ask for start and end year
 start_year = int(input("Enter start year: "))
@@ -37,16 +42,16 @@ for mm_yyyy in list_mm_yyyy:
     try:
         download_main(mm_yyyy, log_dir_path)
     except Exception as e:
-        print(f"Error downloading files: {e}")
+        logging.error(f"Error downloading files: {e}")
     try:
         classify_main(mm_yyyy, log_dir_path)
     except Exception as e:
-        print(f"Error classifying files: {e}")
+        logging.error(f"Error classifying files: {e}")
 # Sample the files
 try:
     mm_yyyy_sampled_files, mm_yyyy_size, mm_yyyy_weight = files_sampler(list_mm_yyyy, num_files, log_dir_path)
 except Exception as e:
-    print(f"Error sampling files: {e}")
+    logging.error(f"Error sampling files: {e}")
 # Save mm_yyyy_size, mm_yyyy_size and mm_yyyy_weight
 import json
 with open("data/mm_yyyy_sampled_files.json", "a") as f:
@@ -58,8 +63,8 @@ with open("data/mm_yyyy_weight.json", "a") as f:
 
 # Loop through the list of months amd extract the data
 for mm_yyyy in list_mm_yyyy:
-    print(f"-"*50)
-    print(f"Processing {mm_yyyy}...")
+    logging.debug(f"-"*50)
+    logging.debug(f"Processing {mm_yyyy}...")
 
     # Extract data
     for mm_yyyy, sampled_files in mm_yyyy_sampled_files.items():
