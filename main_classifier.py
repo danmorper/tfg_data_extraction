8aa0f9bb670c4f86d76ed8135bd76ac7002014ef
Daniel Moreno <danielmoreno@Daniels-MacBook-Air.local>
2024-06-19 10:49:51 +0200

    log_dir missing in main_classifier.py when calling instances

--- main_classifier.py
+++ main_classifier.py
@@ -14,15 +14,15 @@ def main(mm_yyyy: str, log_dir: str):
     
     source_folder_contratacion = f'pdfs_range_{mm_yyyy}'
     logging.debug(f'Classifying contratacion files in {source_folder_contratacion}')
-    classifier_contratacion = ClassifierContratacion(source_folder=source_folder_contratacion)
+    classifier_contratacion = ClassifierContratacion(source_folder=source_folder_contratacion, log_dir=log_dir)
 
     source_folder_anuncio = f'pdfs_range_{mm_yyyy}/contratacion'
     logging.debug(f'Classifying anuncio files in {source_folder_anuncio}')
-    classifier_anuncio = ClassifierAnuncio(source_folder=source_folder_anuncio)
+    classifier_anuncio = ClassifierAnuncio(source_folder=source_folder_anuncio, log_dir=log_dir)
 
     source_folder_formalizacion = f'pdfs_range_{mm_yyyy}/contratacion'
     logging.debug(f'Classifying formalizacion files in {source_folder_formalizacion}')
-    classifier_formalizacion = ClassifierFormalizacion(source_folder=source_folder_formalizacion)
+    classifier_formalizacion = ClassifierFormalizacion(source_folder=source_folder_formalizacion, log_dir=log_dir)
 
     save_execution_time(classifier_contratacion, classifier_anuncio, classifier_formalizacion)
     logging.debug(f'Finished classification for {mm_yyyy}')
